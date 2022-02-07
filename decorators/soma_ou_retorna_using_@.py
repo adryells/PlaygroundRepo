890488c81ac4bc1e23c94ac59bd368066dcf2791
adryells <adryellpaulo@gmail.com>
2022-02-06 22:18:50 -0300

    Decorator de validação se parametro é numero

--- decorators/soma_ou_retorna_using_@.py
+++ decorators/soma_ou_retorna_using_@.py
@@ -1,25 +1,30 @@
-def validates_val_is_a_number(fucn):
-    def validate(x, y):
+def validates_val_is_a_number(func):
+    def validate(x,y):
         if not all(isinstance(val, int) for val in [x,y]):
-            return "One or more values not is a number"
+            print("One or more values not is a number")
+            return
 
+        return func(x,y)
     return validate
 
 
-
-
-@validates_val_is_a_number(soma,1,2)
+@validates_val_is_a_number
 def soma(x, y):
+    print(f"{x} + {y} = {x+y}")
     return x + y
 
-@validates_val_is_a_number()
-def subtrai(x, y):
-    return x - y
 
-@validates_val_is_a_number()
-def multiplica(x, y):
-    return x * y
+s = soma(1,4)
+s = soma(1,"4")
 
-@validates_val_is_a_number()
-def divide(x, y):
-    return x / y
+# @validates_val_is_a_number()
+# def subtrai(x, y):
+#     return x - y
+#
+# @validates_val_is_a_number()
+# def multiplica(x, y):
+#     return x * y
+#
+# @validates_val_is_a_number()
+# def divide(x, y):
+#     return x / y
