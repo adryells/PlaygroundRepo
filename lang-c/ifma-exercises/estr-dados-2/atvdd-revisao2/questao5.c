17d435029d2b39f34b92fc19b4049412bc2faf8a
Paulo Adryell <40843657+adryells@users.noreply.github.com>
2022-03-23 12:19:59 -0300

    Update questao5.c

--- lang-c/ifma-exercises/estr-dados-2/atvdd-revisao2/questao5.c
+++ lang-c/ifma-exercises/estr-dados-2/atvdd-revisao2/questao5.c
@@ -20,7 +20,7 @@ int main(void) {
   if(option == "H"){
     printf("A média harmonica é: %f", (1/n1, 1/n2, 1/n3)/3);
   } else if (option == "P"){
-    printf("A média aritmética é: %f", (n1 * 5, n2 * 3, n3 * 2)/10);
+    printf("A média ponderada é: %f", (n1 * 5, n2 * 3, n3 * 2)/10);
   } else if (option == "A"){
     printf("A média aritmética é: %f", (n1, n2, n3)/3);
   } else {
